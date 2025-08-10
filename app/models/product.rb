7d65970fa379a942c0e8c2b9615ed248e8b76ff7
Feret Thomas <thomas.feret@hotmail.fr>
2025-08-10 17:16:13 +0200

    Add Cloudinary image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :photo
+
   validates :name, presence: true, length: { minimum: 2, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }
   validates :price, presence: true, numericality: { greater_than: 0 }
