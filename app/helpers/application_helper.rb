7ba05ee568db6cf3cf32cefb8b312af56baf7129
Feret Thomas <thomas.feret@hotmail.fr>
2025-09-16 00:58:41 +0200

    Add breadcrumbs import and helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def breadcrumbs(*links)
+    content_tag :nav, class: "breadcrumbs" do
+      content_tag :div, class: "container" do
+        content_tag :ol, class: "breadcrumb" do
+          content_tag(:li, link_to("Accueil", root_path), class: "breadcrumb-item") +
+          links.map do |text, path|
+            if path
+              content_tag(:li, link_to(text, path), class: "breadcrumb-item")
+            else
+              content_tag(:li, text, class: "breadcrumb-item active")
+            end
+          end.join.html_safe
+        end
+      end
+    end
+  end
 end
