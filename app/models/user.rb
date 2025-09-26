5d9a99b2214fa70fd1f05efe352905b582d6b2cb
Feret Thomas <thomas.feret@hotmail.fr>
2025-09-26 16:12:23 +0200

    Add favorites relationships to User and Product models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :products, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_products, through: :favorites, source: :product
+
+  # Méthode helper pour savoir si un produit est en favori
+  def has_favorite?(product)
+    favorites.exists?(product: product)
+  end
 end
