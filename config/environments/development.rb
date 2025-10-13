4468e8a3b6d178bae4ddf78a4bf0840f7a310db6
Feret Thomas <thomas.feret@hotmail.fr>
2025-10-13 14:42:51 +0200

    Configure letter_opener_web as email delivery method

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
 
   config.active_storage.service = :cloudinary
 
-  # ✉️ CONFIGURATION MAILER
-  config.action_mailer.delivery_method = :letter_opener
+  # ✉️ CONFIGURATION MAILER - Letter Opener Web
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
