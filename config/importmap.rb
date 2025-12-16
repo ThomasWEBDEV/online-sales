db0001199c0d08e554b97e6d84f4579c3a4f0f97
Feret Thomas <thomas.feret@hotmail.fr>
2025-12-16 17:40:15 +0100

    add mobile_menu to importmap for Heroku deployment

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap" # @5.3.8
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+
+# Mobile menu
+pin "mobile_menu", to: "mobile_menu.js"
