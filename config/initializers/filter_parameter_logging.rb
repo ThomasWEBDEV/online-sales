24d40e7b7c539e5d73de016e043e7adf802caf9d
Feret Thomas <thomas.feret@hotmail.fr>
2026-01-01 15:15:57 +0100

    security: Add comprehensive parameter filtering for sensitive data in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,27 @@
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
+
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  # 🔒 Authentication & Authorization
+  :passw, :password, :password_confirmation,
+  :secret, :token, :_key, :api_key, :access_token, :refresh_token,
+  :crypt, :salt, :certificate, :otp, :ssn,
+
+  # 🔒 Payment Information
+  :card_number, :cvv, :cvc, :card_cvv, :card_cvc,
+  :credit_card, :stripe_token, :stripe_key,
+  :iban, :bic, :account_number,
+
+  # 🔒 Personal Data (RGPD)
+  :phone, :mobile, :telephone,
+  :address, :street, :postal_code, :zip_code,
+  :social_security, :passport, :driver_license,
+
+  # 🔒 Cloudinary & AWS
+  :cloudinary_url, :cloudinary_secret,
+  :aws_access_key_id, :aws_secret_access_key,
+
+  # 🔒 Stripe Webhooks
+  :stripe_signature, :webhook_secret
 ]
