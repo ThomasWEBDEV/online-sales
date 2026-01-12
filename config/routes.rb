b661ff560f19a3b843b4a3d3a8b75c76b0162637
Feret Thomas <thomas.feret@hotmail.fr>
2026-01-12 16:35:02 +0100

    config: Update routes to use custom registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'products#index'
 
   # Letter Opener Web (emails en dev)
